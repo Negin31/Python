160aec7c43b6dbb5e4c8bbc6e75d073794d0f66f
Negin31 <48496752+Negin31@users.noreply.github.com>
2019-07-23 17:14:12 +0300

    Update loto.py

--- loto.py
+++ loto.py
@@ -1,19 +1,32 @@
 #Козловцев Сергей
 from random import sample
 class Card:
-    def go_random():
-        a = sample(range(0,90),27)
-        return a
-    def cart_ran(self,a):
+    def __init__(self):
+        self.cart = sample(range(0,90),27)
+        self.is_true = True
+    def cart_print(self,a = self.cart):
         print('{} {} {} {} {} {} {} {} {}\n{} {} {} {} {} {} {} {} {}\n{} {} {} {} {} {} {} {} {}'.format(*a))
-class RandDo:
-    def rannum(self, a = list, r = int, item = list):
-        f = a(index(r)
-        print(f)
-        if input() ==1:
-            item(f) = '  '
+class Player(Card):
+    def chenge(self):
+        if Numirator.val in self.cart and a = 1:
 
 a = sample(range(0,90),90)
 while
 print('')
 print(Card.cart_ran(Card,a))
+=======
+class Bot(Card):
+    def
+class Numirator:
+    def __init__(self):
+        self.list = sample(range(0,90),90)
+        self.i = 0
+        self.val = 0
+    def num(self):
+        while (Player.is_true == True) and (Bot.is_true == True):
+            self.val = self.list(self.i)
+            Player.chenge()
+            self.i = self.i + 1
+player = Card()
+bot = Card()
+print(player.cart)
