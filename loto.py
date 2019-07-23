4dd0709302ea19989c66e7d3b74fd654273001db
Negin31 <48496752+Negin31@users.noreply.github.com>
2019-07-23 15:49:08 +0300

    loto

---
+++ loto.py
@@ -0,0 +1,18 @@
+from random import sample
+class Card:
+    def go_random():
+        a = sample(range(0,90),27)
+        return a
+    def cart_ran(self,a):
+        print('{} {} {} {} {} {} {} {} {}\n{} {} {} {} {} {} {} {} {}\n{} {} {} {} {} {} {} {} {}'.format(*a))
+class RandDo:
+    def rannum(self, a = list, r = int, item = list):
+        f = a(index(r)
+        print(f)
+        if input() ==1:
+            item(f) = '  '
+
+a = sample(range(0,90),90)
+while
+print('')
+print(Card.cart_ran(Card,a))
